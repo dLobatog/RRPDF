73407126a82df8e380fc01f0c99cce4f695af725
eLobato <elobatocs@gmail.com>
2012-04-10 18:23:22 -0700

    Basic gem structure

---
+++ test/dummy/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Dummy::Application.config.secret_token = 'c0ae263763ba0b3c39bde8f952fd6e81f42b3424a7d84f488ec626f0eec06acb1c6850b6b43784ff1075a846964e54afd5225440a765efa72423cca14cc95b42'
