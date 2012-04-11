8b1035098999f2e271c1553944751e785e963494
Daniel Lobato Garcia <elobatocs at google.com>
2012-04-10 18:45:25 -0700

    Registered PDF as a MIME Rails path

--- test/pdf_renderer_test.rb
+++ test/pdf_renderer_test.rb
@@ -1,6 +1,11 @@
 require 'test_helper'
 
 class PdfRendererTest < ActiveSupport::TestCase
+  test "pdf mime type" do
+    assert_equal :pdf, Mime::PDF.to_sym
+    assert_equal "application/pdf", Mime::PDF.to_s
+  end
+  
   test "truth" do
     assert_kind_of Module, PdfRenderer
   end
