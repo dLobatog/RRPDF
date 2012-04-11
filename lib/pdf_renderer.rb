7baf0b1f034a0b5f010032a7ea39d316a2db813b
eLobato <elobatocs@gmail.com>
2012-04-10 18:49:37 -0700

    Used Prawn to render the PDF and send it

--- lib/pdf_renderer.rb
+++ lib/pdf_renderer.rb
@@ -1,4 +1,13 @@
 require 'action_controller'
+require 'prawn'
+
 module PdfRenderer
-  Mime::Type.register "appliction/pdf", :pdf
+  Mime::Type.register "application/pdf", :pdf
+  ActionController::Renderers.add :pdf do |filename,options|
+    pdf = Prawn::Document.new
+    pdf.text render_to_string(options)
+    send_data(pdf.render, :filename => "#{filename}.pdf", 
+             :type => "application/pdf", :disposition => "attachment")
+  end
+
 end
