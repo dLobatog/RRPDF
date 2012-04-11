abc3fff10cda2ec8fa1aa1d686845b2049559aa2
eLobato <elobatocs@gmail.com>
2012-04-10 18:41:54 -0700

    Integration test with Capybara

--- test/integration/navigation_test.rb
+++ test/integration/navigation_test.rb
@@ -1,6 +1,22 @@
 require 'test_helper'
 
 class NavigationTest < ActiveSupport::IntegrationCase
+  test 'pdf request sends a pdf as a file' do
+    visit home_path
+    click_link 'PDF'
+
+    assert_equal 'binary', headers['Content-Transfer-Encoding']
+    assert_equal 'attachment; filename="contents.pdf"',
+                  headers['Content-Disposition']
+    assert_equal 'application/pdf', headers['Content-Type']
+    assert_match /Prawn/, page.body
+  end
+
+  protected 
+  def headers
+    page.response_headers
+  end
+
   test "truth" do
     assert_kind_of Dummy::Application, Rails.application
   end
